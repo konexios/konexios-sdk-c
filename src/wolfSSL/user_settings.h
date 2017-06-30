df14fab2835bd97febfff857d0237e6947a67f60
Danila V. Demidow <dandemidow@gmail.com>
2017-06-30 10:09:50 +0300

    the nucleo fix

--- src/wolfSSL/user_settings.h
+++ src/wolfSSL/user_settings.h
@@ -32,39 +32,49 @@
 
 // FIXME rm MBED this for the nucleo
 #elif defined(__MBED__) && !defined(__semiconductor__)
-
-    #define MBED
-//    #define WOLFSSL_CMSIS_RTOS
-    #define WOLFSSL_USER_IO
-    #define NO_WRITEV
-    #define NO_DEV_RANDOM
-    #define HAVE_ECC
-    #define HAVE_AESGCM
-    
-    #define WOLFSSL_SHA384
-    #define WOLFSSL_SHA512
-    #define HAVE_CURVE25519
-    #define HAVE_ED25519   /* with HAVE_SHA512 */
-    //#define HAVE_POLY1305
-    //#define HAVE_CHACHA
-    //#define HAVE_ONE_TIME_AUTH
-    #define NO_SESSION_CACHE // For Small RAM
-    //#define IGNORE_KEY_EXTENSIONS
-    #define NO_WOLFSSL_DIR  
+#define MBED
+#define WOLFSSL_USER_IO
+#define NO_WRITEV
+#define NO_DEV_RANDOM
+#define NO_WOLFSSL_DIR
+#define NO_SHA512
+#define NO_DH
+#define NO_DSA
+//#define NO_RSA
+#define NO_RC4
+#define NO_DES
+#define NO_DES3
+#define NO_RABBIT
+//#define NO_AES
+#define NO_ECC256
+#define HAVE_ECC
+//#define NO_ECC_DHE
+#define NO_HC128
+#define NO_PSK
+#define NO_MD2
+#define NO_MD4
+//#define NO_MD5
+#define NO_OLD_TLS
+#define NO_PWDBASED
+#define NO_SKID
+#define NO_WOLFSSL_SERVER
+#define NO_SESSION_CACHE // For Small RAM
+//#define IGNORE_KEY_EXTENSIONS
+#define NO_WOLFSSL_DIR
 //    #define DEBUG_WOLFSSL
-    #define WOLFSSL_STATIC_RSA
-    #define HAVE_SUPPORTED_CURVES
-    #define HAVE_TLS_EXTENSIONS
-    #define SIZEOF_LONG_LONG  8
-    /* Options for Sample program */
-    //#define WOLFSSL_NO_VERIFYSERVER
-    //#define NO_FILESYSTEM
+#define WOLFSSL_STATIC_RSA
+#define HAVE_SUPPORTED_CURVES
+#define HAVE_TLS_EXTENSIONS
+#define SIZEOF_LONG_LONG  8
+/* Options for Sample program */
+//#define WOLFSSL_NO_VERIFYSERVER
+//#define NO_FILESYSTEM
 //    #define HAVE_TM_TYPE
-    #ifndef WOLFSSL_NO_VERIFYSERVER
-        #define TIME_OVERRIDES
-        #define XTIME time
-        #define XGMTIME localtime
-    #endif
+#ifndef WOLFSSL_NO_VERIFYSERVER
+#define TIME_OVERRIDES
+#define XTIME time
+#define XGMTIME localtime
+#endif
 
 #elif defined(__linux__)
 #define WOLFSSL_USER_IO
