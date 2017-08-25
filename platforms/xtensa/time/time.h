83536dea0bf8b536a753af0d2967a9b44523bdcf
Danila V. Demidow <dandemidow@gmail.com>
2017-08-25 21:10:59 +0300

    xtensa: struct tm fix

--- platforms/xtensa/time/time.h
+++ platforms/xtensa/time/time.h
@@ -17,6 +17,23 @@
 #if defined(time)
 #  undef time
 #endif
+
+// from xtensa time.h -nostdlib
+struct tm
+{
+  int   tm_sec;
+  int   tm_min;
+  int   tm_hour;
+  int   tm_mday;
+  int   tm_mon;
+  int   tm_year;
+  int   tm_wday;
+  int   tm_yday;
+  int   tm_isdst;
+};
+
+typedef long suseconds_t;
+
 struct timezone { int zone; };
 time_t time(time_t *timer);
 int gettimeofday(struct timeval *tv, struct timezone *tz);
