f3ff2968b323858490b225ac3833361a3f9eddc0
Danila Demidov <dandemidow@gmail.com>
2018-03-08 23:54:21 +0300

    fix http_response tests

--- test/test/test_http_response.c
+++ test/test/test_http_response.c
@@ -22,28 +22,34 @@ void tearDown(void)
 
 static http_response_t _test_response;
 
+extern int default_set_payload_handler(void *r,
+                            property_t buf,
+                            int size);
+
+extern int default_add_payload_handler(void *r,
+                            property_t payload,
+                            int size);
+
 void test_http_response_init(void) {
-    http_response_init(&_test_response, GET, "http://api.arrowconnect.io:80/api/v1/kronos/gateways");
-    TEST_ASSERT(_test_request._response_payload_meth._p_add_handler);
-    TEST_ASSERT(_test_request._response_payload_meth._p_set_handler);
-    TEST_ASSERT_EQUAL_INT(0, _test_request.is_corrupt);
-    TEST_ASSERT_EQUAL_STRING("api.arrowconnect.io", P_VALUE(_test_request.host));
-    TEST_ASSERT_EQUAL_INT(80, _test_request.port);
-    TEST_ASSERT_EQUAL_STRING("GET", P_VALUE(_test_request.meth));
-    TEST_ASSERT_EQUAL_STRING("http", P_VALUE(_test_request.scheme));
-    TEST_ASSERT( IS_EMPTY(_test_request.payload.buf) );
-    TEST_ASSERT_EQUAL_INT(0, _test_request.payload.size);
-    TEST_ASSERT( ! _test_request.header );
-    TEST_ASSERT( IS_EMPTY(_test_request.content_type.value) );
-    TEST_ASSERT( IS_EMPTY(_test_request.content_type.key) );
-    TEST_ASSERT( ! _test_request.content_type.node.next );
+    _payload_meth_t def_handler = { default_set_payload_handler,
+                                    default_add_payload_handler };
+    http_response_init(&_test_response, &def_handler);
+    TEST_ASSERT(_test_response._p_meth._p_add_handler);
+    TEST_ASSERT(_test_response._p_meth._p_set_handler);
+    TEST_ASSERT_EQUAL_INT(0, _test_response.is_chunked);
+    TEST_ASSERT( IS_EMPTY(_test_response.payload.buf) );
+    TEST_ASSERT_EQUAL_INT(0, _test_response.payload.size);
+    TEST_ASSERT( ! _test_response.header );
+    TEST_ASSERT( IS_EMPTY(_test_response.content_type.value) );
+    TEST_ASSERT( IS_EMPTY(_test_response.content_type.key) );
+    TEST_ASSERT( ! _test_response.content_type.node.next );
 }
 
 void test_http_request_add_header(void) {
-    http_request_add_header(&_test_request, p_heap("hello"), p_heap("kokoko"));
-    TEST_ASSERT( _test_request.header );
+    http_response_add_header(&_test_response, p_heap("hello"), p_heap("kokoko"));
+    TEST_ASSERT( _test_response.header );
     property_map_t *tmp = NULL;
-    for_each_node(tmp, _test_request.header, property_map_t) {
+    for_each_node(tmp, _test_response.header, property_map_t) {
         TEST_ASSERT_EQUAL_STRING("hello", P_VALUE(tmp->key));
         TEST_ASSERT_EQUAL_STRING("kokoko", P_VALUE(tmp->value));
     }
