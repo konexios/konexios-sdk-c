88e3b9c6c8aa1325eb1d64171976323b84551919
Danila Demidov <dandemidow@gmail.com>
2018-08-17 12:44:48 +0300

    test fix

--- test/src/http_cb.c
+++ test/src/http_cb.c
@@ -73,7 +73,7 @@ ssize_t recv_cb(int sockfd, void *buf, size_t len, int flags, int count) {
         memcpy(buf, http_r[http_resp_cout].text + resp_index, size);
 //         printf("r[%s] %d\r\n", buf, size);
         resp_index += size;
-//        printf("---- last %d %d\r\n", resp_index, http_r[http_resp_cout].http_size);
+//        printf("---- la
         if ( resp_index == http_r[http_resp_cout].http_size &&
              http_resp_cout < 10 ) {
             // next expected message
