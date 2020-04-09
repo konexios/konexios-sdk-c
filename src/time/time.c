38852a3f5c78ecfdef29bb5a54fe1a04f3fc9ada
TUYEN Nguyen <tuyennh@fsoft.com.vn>
2020-04-09 14:46:44 +0700

    update timestamp function

--- src/time/time.c
+++ src/time/time.c
@@ -54,7 +54,20 @@ void timestamp_string(acn_timestamp_t *ts, char *s) {
              ts->msec);
 }
 
-
-void timestamp(acn_timestamp_t *ts) {
-
+void __attribute_weak__ timestamp(acn_timestamp_t *ts){
+    struct tm *tmp;
+    int ms;
+    time_t s = time(NULL);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    tmp = gmtime(&s);
+    ms = (tv.tv_usec/1000)%1000;
+    ts->year = 1900 + tmp->tm_year;
+    ts->mon = 1 + tmp->tm_mon;
+    ts->day = tmp->tm_mday;
+    ts->hour = tmp->tm_hour;
+    ts->min = tmp->tm_min;
+    ts->sec = tmp->tm_sec;
+    ts->msec = ms;
 }
+
